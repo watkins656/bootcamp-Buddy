bf14f6b58befaf36a45eab0e184b5c879e2cceb1
Dustin Watkins <watkins656@gmail.com>
2018-11-01 11:18:37 -0500

    jaws

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+let dotenv = require("dotenv").config();
+let mysql = require("mysql");
+
+let connection;
+
+if(process.env.JAWSDB_URL){
+
+connection=mysql.createConnection(process.env.JAWSDB_URL)}
+else{
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: mySQLPassword,
+    database: "bootcamp_Buddy"
+  });
+}
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+});
 // Dependencies
 // =============================================================
 var express = require("express");
